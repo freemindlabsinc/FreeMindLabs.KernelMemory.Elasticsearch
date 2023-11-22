06da093c792e18a21ebbeb8dcb5a8deb2a20f80e
Alessandro Federici <afederici75@users.noreply.github.com>
2023-11-22 17:13:42 -0500

    Cleaned up a bit

--- Tests/ElasticsearchMemoryStoreTests.cs
+++ Tests/ElasticsearchMemoryStoreTests.cs
@@ -2,6 +2,7 @@ using Elastic.Clients.Elasticsearch;
 using Elastic.Transport;
 using FreeMindLabs.SemanticKernel.Connectors.Elasticsearch;
 using Microsoft.Extensions.Configuration;
+using Microsoft.SemanticKernel.Memory;
 
 namespace Tests;
 
@@ -16,21 +17,29 @@ public class ElasticsearchMemoryStoreTests
     [Fact]
     public async Task ShouldCreateCollection()
     {
-        IConfigurationRoot cfg = GetConfiguration();
-        using var settings = new ElasticsearchClientSettings(new Uri(cfg["Elasticsearch:Url"]));
+        var ms = GetMemoryStore();
 
-        var auth = new BasicAuthentication(cfg["Elasticsearch:UserName"], cfg["Elasticsearch:Password"]);
-        settings.Authentication(auth)
-            // TODO: Not sure why I need this. Verify configuration maybe?
-            .ServerCertificateValidationCallback((sender, certificate, chain, errors) => true)
-            .CertificateFingerprint(cfg["Elasticsearch:CertificateFingerPrint"]);
-
-        var ms = new ElasticsearchMemoryStore(settings);
         await ms.CreateCollectionAsync("test")
                 .ConfigureAwait(false);
     }
 
-    #region To move elsewhere
+    #region To move elsewhere/refactor
+
+    static private IMemoryStore GetMemoryStore()
+    {
+        IConfigurationRoot cfg = GetConfiguration();
+
+        // TODO: IDisposable is weird...
+        using var settings = new ElasticsearchClientSettings(new Uri(cfg["Elasticsearch:Url"]));
+
+        settings.Authentication(new BasicAuthentication(cfg["Elasticsearch:UserName"], cfg["Elasticsearch:Password"]))
+            // TODO: Not sure why I need this. Verify configuration maybe?
+            .ServerCertificateValidationCallback((sender, certificate, chain, errors) => true)
+            .CertificateFingerprint(cfg["Elasticsearch:CertificateFingerPrint"]);
+
+        IMemoryStore ms = new ElasticsearchMemoryStore(settings);
+        return ms;
+    }
 
     private static IConfigurationRoot GetConfiguration()
     {
