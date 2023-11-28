c3bad9f523a414cefc4ac3960074dda20e091446
Alessandro Federici <afederici75@users.noreply.github.com>
2023-11-28 11:36:50 -0500

    Broken...

--- FreeMindLabs.KernelMemory.MemoryStorage.Elasticsearch/ElasticsearchMemory.cs
+++ FreeMindLabs.KernelMemory.MemoryStorage.Elasticsearch/ElasticsearchMemory.cs
@@ -8,43 +8,56 @@ using System.Threading.Tasks;
 
 namespace FreeMindLabs.KernelMemory.MemoryStorage.Elasticsearch;
 
+/// <summary>
+/// TBC
+/// </summary>
 public class ElasticsearchMemory : IVectorDb
 {
+    /// <summary>
+    /// TBC
+    /// </summary>
     public ElasticsearchMemory()
     {
             
     }
 
+    /// <inheritdoc/>
     public Task CreateIndexAsync(string index, int vectorSize, CancellationToken cancellationToken = default)
     {
         throw new NotImplementedException();
     }
 
+    /// <inheritdoc/>
     public Task DeleteAsync(string index, MemoryRecord record, CancellationToken cancellationToken = default)
     {
         throw new NotImplementedException();
     }
 
+    /// <inheritdoc/>
     public Task DeleteIndexAsync(string index, CancellationToken cancellationToken = default)
     {
         throw new NotImplementedException();
     }
 
+    /// <inheritdoc/>
     public Task<IEnumerable<string>> GetIndexesAsync(CancellationToken cancellationToken = default)
     {
         throw new NotImplementedException();
     }
 
+    /// <inheritdoc/>
     public IAsyncEnumerable<MemoryRecord> GetListAsync(string index, ICollection<MemoryFilter>? filters = null, int limit = 1, bool withEmbeddings = false, CancellationToken cancellationToken = default)
     {
         throw new NotImplementedException();
     }
 
+    /// <inheritdoc/>
     public IAsyncEnumerable<(MemoryRecord, double)> GetSimilarListAsync(string index, Embedding embedding, ICollection<MemoryFilter>? filters = null, double minRelevance = 0, int limit = 1, bool withEmbeddings = false, CancellationToken cancellationToken = default)
     {
         throw new NotImplementedException();
     }
 
+    /// <inheritdoc/>
     public Task<string> UpsertAsync(string index, MemoryRecord record, CancellationToken cancellationToken = default)
     {
         throw new NotImplementedException();
--- ServerlessUploadAndQuestions/Program.cs
+++ ServerlessUploadAndQuestions/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿
+// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+var memory = new KernelMemoryBuilder();
+//    .WithOpenAIDefaults(Env.Var("OPENAI_API_KEY"))
+//    // .FromAppSettings() => read "KernelMemory" settings from appsettings.json (if available), see https://github.com/microsoft/kernel-memory/blob/main/dotnet/Service/appsettings.json as an example
+//    // .WithAzureBlobsStorage(new AzureBlobsConfig {...})                                              => use Azure Blobs
+//    // .WithAzureCognitiveSearch(Env.Var("ACS_ENDPOINT"), Env.Var("ACS_API_KEY"))                      => use Azure Cognitive Search
+//    // .WithQdrant("http://127.0.0.1:6333")                                                            => use Qdrant docker
+//    // .WithAzureFormRecognizer(Env.Var("AZURE_COG_SVCS_ENDPOINT"), Env.Var("AZURE_COG_SVCS_API_KEY")) => use Azure Form Recognizer OCR
+//    .Build<MemoryServerless>();
