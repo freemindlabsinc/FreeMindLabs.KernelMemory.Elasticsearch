add2b61b42ec72c4f6d0b104f977683675f84f77
Alessandro Federici <afederici75@users.noreply.github.com>
2023-12-01 15:07:53 -0500

    -Improvements oin configuration and extensions

---
+++ ElasticsearchMemoryStorage/ElasticsearchConfigurationException.cs
@@ -0,0 +1,20 @@
+﻿// Copyright (c) Free Mind Labs, Inc. All rights reserved.
+
+using System;
+
+namespace FreeMindLabs.KernelMemory.Elasticsearch;
+
+/// <summary>
+/// Exception thrown when the Elasticsearch configuration is invalid in appSettings, secrets, etc.
+/// </summary>
+public class ElasticsearchConfigurationException : ElasticsearchException
+{
+    /// <inheritdoc />
+    public ElasticsearchConfigurationException() { }
+
+    /// <inheritdoc />
+    public ElasticsearchConfigurationException(string message) : base(message) { }
+
+    /// <inheritdoc />
+    public ElasticsearchConfigurationException(string message, Exception? innerException) : base(message, innerException) { }
+}
--- UnitTests/Startup.cs
+++ UnitTests/Startup.cs
@@ -18,35 +18,26 @@ public class Startup
     {
         this._configuration = new ConfigurationBuilder()
             .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
-            .AddUserSecrets(Assembly.GetExecutingAssembly())
+            .AddUserSecrets(Assembly.GetExecutingAssembly()) // Same secrets as SK and KM :smile:
             .Build() ?? throw new ArgumentException();
     }
 
     public void ConfigureServices(IServiceCollection services)
     {
-        const string EsSection = "Elasticsearch";
-        var esConfig = this._configuration.GetSection(EsSection).Get<ElasticsearchConfig>() ?? throw new ArgumentException(EsSection);
-
-        using var esSettings = new ElasticsearchClientSettings(new Uri(esConfig.Endpoint!));
-
-        var auth = new BasicAuthentication(
-            username: this._configuration["Elasticsearch:UserName"]!,
-            password: this._configuration["Elasticsearch:Password"]!);
-        esSettings.Authentication(auth)
-            // TODO: Not sure why I need this. Verify configuration maybe?
-            .ServerCertificateValidationCallback((sender, certificate, chain, errors) => true)
-            .CertificateFingerprint(this._configuration["Elasticsearch:CertificateFingerPrint"]!);
-
-        esSettings.DisableDirectStreaming(true)
-                  .ThrowExceptions(true);
-
         // ElasticsearchClientSettings
-        services.AddTransient<ElasticsearchClientSettings>(x => esSettings);
+        var esConfig = this._configuration
+            .GetSection(ElasticsearchConfig.DefaultSettingsSection)
+            .Get<ElasticsearchConfig>()
+            .Validate(); // This checks everything is in order.
+
+        services.AddTransient<ElasticsearchClientSettings>(x => esConfig.ToElasticsearchClientSettings());
 
         // Kernel Memory with Elasticsearch
         services.AddTransient<IKernelMemory>(x => new KernelMemoryBuilder()
-            .WithOpenAIDefaults(this._configuration["OpenAI:ApiKey"]!)
+            .WithOpenAIDefaults(this._configuration["OpenAI:ApiKey"] ?? throw new ElasticsearchConfigurationException("OpenAI API Key missing."))
             .WithElasticsearch(esConfig)
             .Build<MemoryServerless>());
+
+        // TODO: Uses OpenAI API Key for now. Make more flexible.
     }
 }
